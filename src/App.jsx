013d9d9f0afcae4fbf8e99f55f11e6063929c344
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-04-03 19:49:33 +0200

    Despliegue

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Navbar from '../src/components/Navbar.jsx';
+import Register from '../src/pages/Register.jsx'
+import Login from '../src/pages/Login.jsx'
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast'
+import { UserContextProvider } from '../content/userContext.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Cortador from './pages/Cortador.jsx';
+import Lista from './pages/Lista.jsx';
+import Redirect from './pages/shortId.jsx';
+
+axios.defaults.baseURL = 'https://recortaenlaces-api.onrender.com'; // CLIENTE https://recortaenlaces.onrender.com
+axios.defaults.withCredentials = true
+
+
+function App() {
+  
+  return (
+    <div className='app'>
+    <UserContextProvider>
+    <Navbar />
+    <Toaster position='bottom-right' toastOptions={{duration: 2000}}/> 
+    <Routes>
+      <Route path='/' element={<Cortador />}></Route>
+      <Route path='/register' element={<Register />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/dashboard' element={<Dashboard />}></Route>
+      <Route path='/:id' element={<Redirect />}></Route>
+    </Routes>
+      <Lista></Lista>
+    </UserContextProvider>
+    </div> 
+  )
+}
+
+export default App
