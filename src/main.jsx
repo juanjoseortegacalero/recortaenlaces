fafef31f6a607bedc2ecb7863a959ab365db7f45
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-04-05 17:38:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
     <App />
     </Router>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
+
