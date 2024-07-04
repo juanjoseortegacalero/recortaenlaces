bfcbfede695053bc1f2aee051528219c40409777
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-07-04 20:32:58 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: { 
-      '/api': 'https://recortaenlaces-api-production.up.railway.app'
-    }
-  },
   plugins: [react()]
 })
